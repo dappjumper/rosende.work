a98cd3ffb9f7bdb10966327cc65c3cd16860852d
Tobias Andersen <dappjump@gmail.com>
2020-07-30 21:50:40 +0200

    Initial clone

---
+++ src/layouts/Default.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="layout">
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  min-height:100vh;
+  max-height:100vh;
+  box-sizing:border-box;
+  overflow:hidden;
+}
+
+#app {
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  margin-top:-3rem;
+}
+
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <Layout>
+    <h1 class="logo">Tobias<span>Andersen</span></h1>
+    <h2 class="subtitle"><b>Say hi:</b> tobias@andersen.work</h2>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Portfolio'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+.logo, .subtitle {
+  text-align:center;
+  font-size:6rem;
+  font-weight:200;
+  color:#fafafa;
+  text-shadow:0px 0px 3rem rgba(0,0,0,0.2);
+}
+.subtitle {
+  font-size:2rem;
+}
+.logo span{
+  margin-left:-1rem;
+  position:relative;
+  top:3rem;
+  font-weight:600;
+}
+
+@media only screen and (max-width: 700px) {
+  .logo {
+    font-size:12vw;
+  }
+  .logo span {
+    top:5vw;
+    margin-left:0rem;
+  }
+}
+
+</style>
